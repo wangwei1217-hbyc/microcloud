ee38f38444efa3c6a2401846c3687bb954153903
wangwei <wangwei_hbyc@aliyun.com>
2019-04-08 00:17:17 +0800

    270104D_SpringSecurity安全访问（安全配置模块）

--- microcloud-consumer-80/src/main/java/cn/mldn/microcloud/config/RestConfig.java
+++ microcloud-consumer-80/src/main/java/cn/mldn/microcloud/config/RestConfig.java
@@ -2,13 +2,31 @@ package cn.mldn.microcloud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 /**
  * Created by wangwei on 2019/4/7 0007.
  */
 @Configuration
 public class RestConfig {
+    /** 要进行一个Http头信息配置*/
+    @Bean
+    public HttpHeaders getHttpHeaders(){
+        // 定义一个HTTP的头信息
+        HttpHeaders httpHeaders = new HttpHeaders();
+        // 认证的原始信息
+        String auth = "root:root";
+        // 进行一个加密的处理
+        byte[] encodeAuth = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII")));
+        // 在进行授权的头信息内容配置的时候加密的信息一定要与“Basic”之间有一个空格
+        String authHeader = "Basic " + new String(encodeAuth);
+        httpHeaders.set("Authorization",authHeader);
+        return httpHeaders;
+    }
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
--- microcloud-consumer-80/src/main/java/cn/mldn/microcloud/controller/ConsumerDeptController.java
+++ microcloud-consumer-80/src/main/java/cn/mldn/microcloud/controller/ConsumerDeptController.java
@@ -1,6 +1,9 @@
 package cn.mldn.microcloud.controller;
 
 import cn.mldn.vo.Dept;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,26 +20,37 @@ import java.util.List;
 public class ConsumerDeptController {
     public static final String DEPT_GET_URL = "http://dept-8001.com:8001/dept/get/";
     public static final String DEPT_LIST_URL = "http://dept-8001.com:8001/dept/list/";
-    public static final String DEPT_ADD_URL = "http://dept-8001.com:8001/dept/add/";
+    public static final String DEPT_ADD_URL = "http://dept-8001.com:8001/dept/add";
 
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private HttpHeaders headers;
 
     @RequestMapping(value = "/consumer/dept/get/{id}",method = {RequestMethod.GET})
     public Object getDept(@PathVariable("id") long id){
-        Dept dept = restTemplate.getForObject(DEPT_GET_URL + id, Dept.class);
+//        Dept dept = restTemplate.getForObject(DEPT_GET_URL + id, Dept.class);
+        /**服务端加入安全策略后，客户端请求方式**/
+        Dept dept = restTemplate.exchange(DEPT_GET_URL + id, HttpMethod.GET,
+                new HttpEntity<Object>(headers), Dept.class).getBody();
         return dept;
     }
 
     @RequestMapping(value = "/consumer/dept/list",method = {RequestMethod.GET})
     public Object getDeptList(){
-        List deptList = restTemplate.getForObject(DEPT_LIST_URL, List.class);
+//        List deptList = restTemplate.getForObject(DEPT_LIST_URL, List.class);
+        /**服务端加入安全策略后，客户端请求方式*/
+        List deptList = restTemplate.exchange(DEPT_LIST_URL, HttpMethod.GET,
+                new HttpEntity<Object>(headers), List.class).getBody();
         return deptList;
     }
 
     @RequestMapping(value = "/consumer/dept/add")
     public Object addDept(Dept dept){
-        Boolean flag = restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class);
+//        Boolean flag = restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class);
+        /**服务端加入安全策略后，客户端请求方式*/
+        Boolean flag = restTemplate.exchange(DEPT_ADD_URL, HttpMethod.POST,
+                new HttpEntity<Object>(dept, headers), Boolean.class).getBody();
         return flag;
     }
 }
