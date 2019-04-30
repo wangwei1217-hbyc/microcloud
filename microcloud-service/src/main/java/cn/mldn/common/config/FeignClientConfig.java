f8f8379b72b0aa752b58478f1bf58cfea0492de4
wangwei <wangwei_hbyc@163.com>
2019-04-30 14:50:24 +0800

    Feign接口转换服务-日志开启及请求数据压缩

--- microcloud-service/src/main/java/cn/mldn/common/config/FeignClientConfig.java
+++ microcloud-service/src/main/java/cn/mldn/common/config/FeignClientConfig.java
@@ -1,14 +1,20 @@
 package cn.mldn.common.config;
 
+import feign.Logger;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Created by wangwei on 2019/4/29.
+ * Created by on 2019/4/29.
  */
 @Configuration
 public class FeignClientConfig {
+    /**开启feign的日志追踪(默认是不开启的)***/
+    @Bean
+    public Logger.Level getFeignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
         return new BasicAuthRequestInterceptor("root","root");
