5daed92d19459e329d2256e5c40f2cbc1282706d
wangwei <wangwei_hbyc@aliyun.com>
2019-08-19 23:58:47 +0800

    SpringCloudSleuth数据采集rabbit+mysql持久化

--- microcloud-sleuth-9601/src/main/java/cn/mldn/microcloud/Zipkin_9601_StartSpringApplication.java
+++ microcloud-sleuth-9601/src/main/java/cn/mldn/microcloud/Zipkin_9601_StartSpringApplication.java
@@ -2,10 +2,12 @@ package cn.mldn.microcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+//@EnableZipkinServer  //内存中
+@EnableZipkinStreamServer //流的方式并持久化
 public class Zipkin_9601_StartSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(Zipkin_9601_StartSpringApplication.class,args);
