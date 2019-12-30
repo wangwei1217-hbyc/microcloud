87c2553b455bed97594380ade3ccf5bb0acff05a
wangwei <wangwei_hbyc@aliyun.com>
2019-12-31 00:14:06 +0800

    SpringCloudConfigBus添加说明

--- microcloud-config-server-bus-7201/src/main/java/cn/mldn/microcloud/ConfigServerBus_7201_StartSpringApplication.java
+++ microcloud-config-server-bus-7201/src/main/java/cn/mldn/microcloud/ConfigServerBus_7201_StartSpringApplication.java
@@ -16,6 +16,29 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  /{label}/{application}-{profile}.yml
  /{application}-{profile}.properties
  /{label}/{application}-{profile}.properties
+
+ --SpringCloudBus服务总线：
+ 1、依赖RabbitMq的发布/订阅模式完成微服务对变更配置文件的自动抓取。
+    (SpringCloudBus服务启动后，RabbitMq里面会有一个springCloudBus的topic类型的Exchange)
+ 2、远端GitHub上的配置文件变更以后，需要以POST的方式手动请求SpringCloudBus服务上所暴露的的
+    /bus/refresh地址，之后客户端服务就可以获取到最新的配置信息了：
+    即curl -X POST http://root:root@config-server-bus-7201.com:7201/bus/refresh
+    注意：权限校验时需添加：ACTUATOR角色，否则请求会报错：访问被拒绝
+ public void configGlobal(AuthenticationManagerBuilder auth)throws Exception {
+     auth.inMemoryAuthentication().withUser("root").password("root")
+     .roles("admin","ACTUATOR").and().withUser("hu").password("hu")
+     .roles("admin", "user","ACTUATOR");
+ }
+ 3、如果想获取配置文件信息变更服务追踪信息的话，可以访问SpringCloudBus服务上的“/trace”地址，获取相关信息。
+  3.1条件：
+     -加入依赖：
+ <dependency>
+     <groupId>org.springframework.boot</groupId>
+     <artifactId>spring-boot-starter-actuator</artifactId>
+ </dependency>
+     -SpringCloudBus服务的application.yml上添加如下配置:
+        spring.cloud.bus.trace.enabled=true
+
  */
 @SpringBootApplication
 @EnableConfigServer  //配置中心服务端注解
